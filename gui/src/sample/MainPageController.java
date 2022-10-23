2914ae7b3ad591fa652e5c695398663703c45d06
Paige <paigemachulsky@gmail.com>
2022-10-23 15:56:42 -0400

    Added fxml for Friends and Playlist Pages

---
+++ gui/src/sample/MainPageController.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainPageController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    public void logout(ActionEvent event) {
+
+    }
+
+}
