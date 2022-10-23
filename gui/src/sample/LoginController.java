f9c5f6264df3ecf7dbeacb29680b1fa2e402634b
Adrienne Deslongchamps <ad4361@rit.edu>
2022-10-23 14:12:59 -0400

    Committed controllers and fxml files

---
+++ gui/src/sample/LoginController.java
@@ -0,0 +1,55 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button goBackButton;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Label loginMessageLabel;
+
+    private void validateLogin() {
+
+    }
+
+
+    public void switchToWelcomeScene(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Welcome.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void login(ActionEvent event) throws IOException {
+
+        if (usernameField.getText().isBlank() || passwordField.getText().isBlank()) {
+            loginMessageLabel.setText("Please enter username and password.");
+        }
+
+    }
+
+}
